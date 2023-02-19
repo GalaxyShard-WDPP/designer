63ec2832cddd5c0b346a843f08740f03bf45fffe
GalaxyShard <swift.galaxyshard@gmail.com>
2023-02-19 01:04:33 -0500

    added background section

--- index.js
+++ index.js
@@ -1,13 +1,24 @@
 
 
 let tabs = document.getElementsByClassName("tab");
-
-for (const tab of tabs) {
+function setActive(tab) {
+    for (const t of tabs) {
+        let header = t.getElementsByClassName("tab-header")[0];
+        let content = t.getElementsByClassName("tab-content")[0];
+        
+        content.style.display = "none";
+        header.classList.remove("highlight");
+    }
     let header = tab.getElementsByClassName("tab-header")[0];
     let content = tab.getElementsByClassName("tab-content")[0];
 
-    content.style.display = "none";
+    header.classList.add("highlight");
+    content.style.display = "block";
+}
+for (const tab of tabs) {
+    let header = tab.getElementsByClassName("tab-header")[0];
     header.addEventListener("click", () => {
-        content.style.display = (content.style.display==="none") ? "block" : "none";
+        setActive(tab);
     });
 }
+setActive(tabs[0]);
